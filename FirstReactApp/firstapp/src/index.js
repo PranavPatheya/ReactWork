21f6825ffe6b306625a43a1a6a828ef5d4d6f0d1
PranavPatheya <pranavk875@outlook.com>
2022-12-20 21:40:42 +0530

    update

--- FirstReactApp/firstapp/src/index.js
+++ FirstReactApp/firstapp/src/index.js
@@ -6,8 +6,12 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+
+
   <React.StrictMode>
+    <h1>Hello, world!</h1>
     <App />
+    
   </React.StrictMode>
 );
 
