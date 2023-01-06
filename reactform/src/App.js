a8b55bca3791fd7c8f903e452d7b106de669e7cd
PranavPatheya <pranavk875@outlook.com>
2023-01-06 13:53:06 +0530

    Update

---
+++ reactform/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+import UserForm from './Component/UserForm'
+function App() {
+  return (
+    <>
+      <div className='userform'>
+        <UserForm />
+      </div>
+    </>
+  );
+}
+
+export default App;
+
